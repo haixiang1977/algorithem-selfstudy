3c6be87ee34be47a673f0f657a52d3cb5036ba6d
LIU YIYANG : ) <haixiang1977@users.noreply.github.com>
2021-02-27 17:11:57 +0800

    Update binary_tree_impl.cpp

--- data_structure/tree/binary_tree_impl.cpp
+++ data_structure/tree/binary_tree_impl.cpp
@@ -3,17 +3,20 @@
 
 // build binary tree manually
 // g++ -g -O0 -std=c++11 binary_tree_impl.cpp -o binary_tree_impl.out
+// ./binary_tree_impl.out
 // in_order_scan
 // name: node3 value: 3
 // name: node1 value: 1
 // name: node4 value: 4
 // name: node0 value: 0
 // name: node5 value: 5
+// name: node6 value: 6
 // name: node2 value: 2
 // post_order_scan
 // name: node3 value: 3
 // name: node4 value: 4
 // name: node1 value: 1
+// name: node6 value: 6
 // name: node5 value: 5
 // name: node2 value: 2
 // name: node0 value: 0
@@ -24,6 +27,9 @@
 // name: node3 value: 3
 // name: node4 value: 4
 // name: node5 value: 5
+// name: node6 value: 6
+// tree leaf count 3
+// tree depth 3
 
 #include <iostream>
 #include <queue>
@@ -112,19 +118,55 @@ void level_order_scan(TreeNode* node)
     while(!q.empty())
     {
         p = q.front();
-	q.pop();
+        q.pop();
 
         cout << "name: " << p->m_name << " value: " << p->m_val << endl;
 
         if (p->m_left)
         {
-	    q.push(p->m_left);
-	}
-	if (p->m_right)
+            q.push(p->m_left);
+        }
+        if (p->m_right)
         {
             q.push(p->m_right);
-	}
+        }
     }
+}
+
+void calc_leaf_cnt(TreeNode* node, int* count_p)
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+
+    if ((node->m_left == nullptr) && (node->m_right == nullptr))
+    {
+        *count_p += 1;
+        return;
+    }
+
+    calc_leaf_cnt(node->m_left, count_p);
+    calc_leaf_cnt(node->m_right, count_p);
+
+    return;
+}
+
+// idea behind
+// depth = 1 + max(depth(left sub tree), depth(right sub tree)
+int calc_depth(TreeNode* node)
+{
+    int depth_left, depth_right;
+
+    if (node == nullptr)
+    {
+        return -1;
+    }
+
+    depth_left = calc_depth(node->m_left);
+    depth_right = calc_depth(node->m_right);
+
+    return 1 + max(depth_left, depth_right);
 }
 
 int main()
@@ -133,11 +175,13 @@ int main()
     //           node0
     //    node1         node2
     // node3 node4   node5
+    //                  node 6
 
     // start from leaf first
     TreeNode node3("node3", 3, nullptr, nullptr);
     TreeNode node4("node4", 4, nullptr, nullptr);
-    TreeNode node5("node5", 5, nullptr, nullptr);
+    TreeNode node6("node6", 6, nullptr, nullptr);
+    TreeNode node5("node5", 5, nullptr, &node6);
 
     TreeNode node1("node1", 1, &node3, &node4);
     TreeNode node2("node2", 2, &node5, nullptr);
@@ -156,7 +200,16 @@ int main()
     cout << "level_order_scan" << endl;
     level_order_scan(&node0);
 
+    // calculate the leaf count
+    int count = 0;
+    calc_leaf_cnt(&node0, &count);
+    printf("tree leaf count %d\n", count);
+
+    // cacluate the depth of the tree
+    int depth = 0;
+    depth = calc_depth(&node0);
+    printf("tree depth %d\n", depth);
+
     return 0;
 }
-
 
