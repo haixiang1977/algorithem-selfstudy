bf35f702ac18a462a64cd2cc76e8fe89865a5d82
LIU YIYANG : ) <haixiang1977@users.noreply.github.com>
2021-03-30 10:57:23 +0800

    Update graph_map_impl.cpp

--- data_structure/graph/graph_map_impl.cpp
+++ data_structure/graph/graph_map_impl.cpp
@@ -30,7 +30,9 @@
 // E  -1 1  -1 -1 -1
 
 // g++ -g -O0 graph_map_impl.cpp -o graph_map_impl.out -std=c++11
-// ./graph_vector_impl.out
+// ./graph_map_impl.out
+// number of vertex 5
+// number of edges 6
 // vertex label: A
 // B:4 C:7
 // vertex label: B
@@ -41,6 +43,22 @@
 // 
 // vertex label: E
 // B:1
+// neighbor of C: A B D
+// erase edge from C to B
+// vertex label: A
+// B:4 C:7
+// vertex label: B
+// C:2
+// vertex label: C
+// D:2
+// vertex label: D
+// 
+// vertex label: E
+// B:1
+// weight from A to C 7
+// weight from A to C 9
+// in degree of C 2
+// out degree of C 1
 #include <iostream>
 #include <string>
 #include <map>
@@ -132,34 +150,116 @@ bool GRAPH::empty()
 
 void GRAPH::erase_edge(std::string v_i, std::string v_j)
 {
+    auto it = m_label_map.find(v_i);
+    int v_i_id = it->second;
+
+    it = m_label_map.find(v_j);
+    int v_j_id = it->second;
+
+    auto itt = m_adjcent_map.find(v_i_id);
+    auto ittt = itt->second.find(v_j_id);
+    itt->second.erase(ittt);
+
     return;
 }
 
 void GRAPH::erase_vertex(std::string v_i)
 {
+    auto it = m_label_map.find(v_i);
+    int v_i_id = it->second;
+
+    auto itt = m_adjcent_map.find(v_i_id);
+    m_adjcent_map.erase(itt);
+
+    for (auto ittt = m_adjcent_map.begin(); ittt != m_adjcent_map.end(); ++ittt)
+    {
+        auto itttt = ittt->second.find(v_i_id);
+        if (itttt != ittt->second.end())
+        {
+            ittt->second.erase(itttt);
+        }
+    }
     return;
 }
 
 int GRAPH::get_weight(std::string v_i, std::string v_j)
 {
-    return -1;
+    int w = -1;
+
+    auto it = m_label_map.find(v_i);
+    int v_i_id = it->second;
+
+    it = m_label_map.find(v_j);
+    int v_j_id = it->second;
+
+    auto itt = m_adjcent_map.find(v_i_id);
+    auto ittt = itt->second.find(v_j_id);
+
+    w = ittt->second;
+
+    return w;
 }
 
 std::set<std::string> GRAPH::get_neighbors(std::string v_i)
 {
     std::set<std::string> s;
 
+    auto it = m_label_map.find(v_i);
+    int v_i_id = it->second;
+
+    auto itt = m_adjcent_map.find(v_i_id);
+    for (auto ittt = itt->second.begin(); ittt != itt->second.end(); ++ittt)
+    {
+        int v_j_d = ittt->first;
+        auto itttt = m_id_map.find(v_j_d);
+        s.insert(itttt->second);
+    }
+
+    for (auto ittt = m_adjcent_map.begin(); ittt != m_adjcent_map.end(); ++ittt)
+    {
+        if (ittt->first != v_i_id)
+        {
+            auto itttt = ittt->second.find(v_i_id);
+            if (itttt != ittt->second.end())
+            {
+                int v_j_d = ittt->first;
+                auto ittttt = m_id_map.find(v_j_d);
+                s.insert(ittttt->second);
+            }
+        }
+    }
+
     return s;
 }
 
 int GRAPH::get_in_degree(std::string v_i)
 {
-    return 0;
+    int cnt = 0;
+    auto it = m_label_map.find(v_i);
+    int v_i_id = it->second;
+
+    for (auto itt = m_adjcent_map.begin(); itt != m_adjcent_map.end(); ++itt)
+    {
+        if (itt->first != v_i_id)
+        {
+            auto ittt = itt->second.find(v_i_id);
+            if (ittt != itt->second.end())
+            {
+                cnt ++;
+            }
+        }
+    }
+
+    return cnt;
 }
 
 int GRAPH::get_out_degree(std::string v_i)
 {
-    return 0;
+    auto it = m_label_map.find(v_i);
+    int v_i_id = it->second;
+
+    auto itt = m_adjcent_map.find(v_i_id);
+    return itt->second.size();
 }
 
 void GRAPH::insert_edge(std::string v_i, std::string v_j, int w)
@@ -187,16 +287,32 @@ void GRAPH::insert_vertex(std::string label, int id)
 
 int GRAPH::get_num_edges()
 {
-    return 0;
+    int size = 0;
+    for (auto it = m_adjcent_map.begin(); it != m_adjcent_map.end(); ++it)
+    {
+        size += it->second.size();
+    }
+    return size;
 }
 
 int GRAPH::get_num_vertices()
 {
-    return 0;
+    return m_label_map.size();
 }
 
 void GRAPH::set_weight(std::string v_i, std::string v_j, int w)
 {
+    auto it = m_label_map.find(v_i);
+    int v_i_id = it->second;
+
+    it = m_label_map.find(v_j);
+    int v_j_id = it->second;
+
+    auto itt = m_adjcent_map.find(v_i_id);
+    auto ittt = itt->second.find(v_j_id);
+
+    ittt->second = w;
+
     return;
 }
 
@@ -249,8 +365,34 @@ int main()
     graph.insert_edge("C", "D", 2);
     // E -> B
     graph.insert_edge("E", "B", 1);
-
+    // get number of edges and vertex
+    std::cout << "number of vertex " << graph.get_num_vertices() << std::endl;
+    std::cout << "number of edges " << graph.get_num_edges() << std::endl;
     graph.dump();
+
+    // get neighbours of "C"
+    std::set<std::string> s = graph.get_neighbors("C");
+    std::cout << "neighbor of C: ";
+    for (auto it = s.begin(); it != s.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    // erase edge from C -> B
+    std::cout << "erase edge from C to B" << std::endl;
+    graph.erase_edge("C", "B");
+    graph.dump();
+
+    // get weight A -> C
+    std::cout << "weight from A to C " << graph.get_weight("A", "C") << std::endl;
+    // set weight A -> C to 9
+    graph.set_weight("A", "C", 9);
+    std::cout << "weight from A to C " << graph.get_weight("A", "C") << std::endl;
+
+    // get vertex C in degree and out degree
+    std::cout << "in degree of C " << graph.get_in_degree("C") << std::endl;
+    std::cout << "out degree of C " << graph.get_out_degree("C") << std::endl;
 
     return 0;
 }
